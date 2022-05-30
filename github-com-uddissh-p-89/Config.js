76f031124e9671faaaa19cc1c408ac2849df9f00
Uddissh <68012232+Uddissh@users.noreply.github.com>
2022-05-30 13:24:24 +0530

    Add files via upload

---
+++ github-com-uddissh-p-89/Config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2M0rxYFbZENRnmdXsQEkjDW0yDmCT9tk",
+  authDomain: "storytelling-4d35e.firebaseapp.com",
+  databaseURL: "https://storytelling-4d35e-default-rtdb.firebaseio.com",
+  projectId: "storytelling-4d35e",
+  storageBucket: "storytelling-4d35e.appspot.com",
+  messagingSenderId: "809181983475",
+  appId: "1:809181983475:web:c9ffb8cab684503b543d86"
+};
+
+export default firebaseConfig
